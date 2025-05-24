c7e85f5240264b47bde3908f60b884e1aa1c9f06
Artha Four <Arumifathina@gmail.com>
2025-05-25 03:21:06 +0700

    adding some function

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 window.axios = axios;
 
+axios.defaults.baseURL = import.meta.env.VITE_APP_URL || '/';
+axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
