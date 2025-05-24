bbb813d4bd81d89a152d01d08e12e3a71de618d5
Artha Four <Arumifathina@gmail.com>
2025-05-25 01:12:45 +0700

    adding some bug fix

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://olivia-g4htendadecwc7fu.canadacentral-01.azurewebsites.net'),
 
     /*
     |--------------------------------------------------------------------------
