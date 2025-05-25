caa964921eb1b0d0403dad8510efe7cee40f970f
Artha Four <Arumifathina@gmail.com>
2025-05-25 12:02:17 +0700

    adding some testing

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 window.axios = axios;
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_URL || '/';
-axios.defaults.withCredentials = true;
 console.log('VITE_APP_URL:', import.meta.env.VITE_APP_URL);
 console.log('axios.defaults.baseURL:', axios.defaults.baseURL);
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
