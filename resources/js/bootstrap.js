546ea93f2179255a346d35224c365f532cf365ee
Artha Four <Arumifathina@gmail.com>
2025-05-25 13:04:03 +0700

    adding some testing

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 window.axios = axios;
 
-axios.defaults.baseURL = window.location.origin
+axios.defaults.baseURL = ''
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
